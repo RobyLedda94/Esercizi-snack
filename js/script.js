f7a8c1dda7658e7262db79079fd03cc498a78d06
RobyLedda94 <robyledda01@yahoo.it>
2025-03-18 11:38:28 +0100

    [UPD] operazioni (somma, differenza, prodotto, divisione)

--- js/script.js
+++ js/script.js
@@ -19,3 +19,31 @@ console.log(eta);
 console.log('CONCATENAZIONE :' + ' ' + 'Ciao mi chiamo' + ' ' + userName + ' ' + 'e ho' + ' ' + eta + ' ' + 'anni');  // concatenazione
 
 console.log(`TEMPLATE LITERALS : Ciao mi chiamo ${userName} e ho ${eta} anni`); // template literals
+
+
+
+// Operazioni matematiche dichiara 2 numeri e stampane la somma, differenza prodotto e divisione
+
+let firstNum = 12;
+
+let secondNum = 2;
+
+// Somma 
+
+let somma = firstNum + secondNum;
+console.log(`La somma tra ${firstNum} + ${secondNum} è uguale a : ${somma}`);
+
+// Differenza
+
+let diff = firstNum - secondNum;
+console.log(`La differenza tra ${firstNum} - ${secondNum} è uguale a : ${diff}`);
+
+// Moltiplicazione
+
+let prod = firstNum * secondNum;
+console.log(`Il prodotto tra ${firstNum} * ${secondNum} è uguale a : ${prod}`);
+
+
+// Divisione 
+let divis = firstNum / secondNum;
+console.log(`La divisione tra ${firstNum} / ${secondNum} è uguale a : ${divis}`);
