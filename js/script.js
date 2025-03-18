3491354121cd08ffdf24876fb7157c415f37f3c5
RobyLedda94 <robyledda01@yahoo.it>
2025-03-18 11:21:03 +0100

    [UPD] dichiaraziosne variabili, concatenazione template literals

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Livello 1 (Variabili, Operatori, Tipi di Dati)
+
+// Crea una variabile e assegnale il tuo nome
+
+// Dichiarazione della variabile name ed assegnazione del suo dato (stringa)
+
+const userName = 'Roberto';
+console.log(userName)
+
+
+// Crea una variabile eta e assegnale latua età
+
+const eta = 31;
+console.log(eta);
+
+
+// Stampa entrambi i valori in console utilizzando la concatenazione e il template literals
+
+console.log('CONCATENAZIONE :' + ' ' + 'Ciao mi chiamo' + ' ' + userName + ' ' + 'e ho' + ' ' + eta + ' ' + 'anni');  // concatenazione
+
+console.log(`TEMPLATE LITERALS : Ciao mi chiamo ${userName} e ho ${eta} anni`); // template literals
